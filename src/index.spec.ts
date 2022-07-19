1a6d995d39eb94e2d2522de3889401b0534fc899
Phongsak Ritpitakphong <jane.makub@gmail.com>
2022-07-19 17:54:38 +0700

    introduce cards

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -17,3 +17,15 @@ test("player1 win by the highest high card", () => {
   expect(pokerHand("2H 3H 4H 8S 5H", "2H 3H 4H 5H 7S")).toBeTruthy();
   expect(pokerHand("2H 3H 8S 4H 5H", "2H 3H 4H 5H 7S")).toBeTruthy();
 });
+
+test("player1 win with 2nd highest high card", () => {
+  expect(pokerHand("2H 3H 4H 8H AS", "2H 3H 4H 6H AS")).toBeTruthy();
+});
+
+test("player1 win with a pair", () => {
+  expect(pokerHand("2H 2D 4H 5H 6S", "2H 3H 4H 6H AS")).toBeTruthy();
+});
+
+test("player1 lose with a smaller pair", () => {
+  expect(pokerHand("2H 2D 4H 5H 6S", "3H 3H 4H 6H AS")).toBeFalsy();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
 export default (handPlayer1: string, handPlayer2: string) => {
-    return highestRank(handPlayer1)>highestRank(handPlayer2);
+    return winByOnePair(new Cards(handPlayer1), new Cards(handPlayer2)) ||
+    winByHighCard(cards(handPlayer1), cards(handPlayer2));
 }
 
-const highestRank = (hand: string): number => {
-    return cards(hand).reduce((currentHighest, card)=> Math.max(rankOfCard(card),currentHighest), 0);
+class Cards{
+    cards: string[]
+
+    constructor(cardString :string) {
+        this.cards = cardString.split(" ");
+    }
+
+    private get ranks() {
+        return ranksOfCards(this.cards)
+    }
+
+    getPair() {
+        return this.ranks.filter((rank) => countRank(this.ranks, rank)=== 2);
+    }
+}
+
+const ranksOfCards = (cards: string[]) => {
+    return cards.map(rankOfCard)
+}
+
+const winByOnePair = (handPlayer1: Cards, handPlayer2: Cards) => {
+    const pairsOfP1 = handPlayer1.getPair()
+    const pairsOfP2 = handPlayer2.getPair()
+    if(pairsOfP1.length >= 1 && pairsOfP2.length >= 1) {
+        return pairsOfP1[0] > pairsOfP2[0]
+    }
+    return pairsOfP1.length >= 1
+}
+
+const countRank = (ranksOfAHand: number[], rank: number) => {
+    return ranksOfAHand.filter((r1) =>r1 === rank).length;
+}
+
+const winByHighCard = (handPlayer1: string[], handPlayer2: string[]) => {
+    const cardsP1 = removeDuplicateRanks(handPlayer1, handPlayer2);
+    const cardsP2 = removeDuplicateRanks(handPlayer2, handPlayer1);
+    return highestRank(cardsP1)>highestRank(cardsP2);
+}
+
+const removeDuplicateRanks = (hand1: string[], hand2: string[]) => {
+    return hand1.filter((card) => !ranksOfCards(hand2).includes(rankOfCard(card)));
+}
+
+const highestRank = (cards: string[]): number => {
+    return ranksOfCards(cards)
+    .reduce((currentHighest, rank) =>
+     Math.max(rank,currentHighest), 0);
 }
 
 const cards = (hand: string): string[] =>{
